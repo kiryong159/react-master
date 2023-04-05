026a137282f872c2a944264c442fbbe0f5c93208
kiryong159 <kiryong159@naver.com>
2023-04-05 19:03:06 +0900

    3.3

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <div>
+      <Circle text="asdf" bgColor="teal" borderColor="black" />
+      <Circle bgColor="yellow" />
+    </div>
+  );
+}
+
+export default App;
