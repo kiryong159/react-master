f893b37b79b02e6e3d78ad9f19159d60d22f01e3
kiryong159 <kiryong159@naver.com>
2023-04-06 12:17:56 +0900

    이론끝

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
-import Circle from "./Circle";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setValue(event.currentTarget.value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  };
   return (
-    <div>
-      <Circle text="asdf" bgColor="teal" borderColor="black" />
-      <Circle bgColor="yellow" />
-    </div>
+    <Container>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="username"
+          value={value}
+          onChange={onChange}
+        />
+        <input type="submit" value="submit" />
+      </form>
+      <H1>1515</H1>
+    </Container>
   );
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,10 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "whitesmoke",
+  textColor: "#111",
+};
+export const darkTheme: DefaultTheme = {
+  bgColor: "#111",
+  textColor: "whitesmoke",
+};
